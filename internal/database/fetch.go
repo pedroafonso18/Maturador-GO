cb1ab3e3598c9bc8b36a5df52a638592fefde63f
Pedro Afonso <pedroafonsoprogramador@gmail.com>
2025-02-19 16:47:14 -0300

    feat: Adicionada a funcionalidade de criar o database caso o user não tenha.

--- internal/database/fetch.go
+++ internal/database/fetch.go
@@ -17,7 +17,14 @@ type Instance struct {
 func FetchConnections() ([]Instance, error) {
 	conn, err := GetConnection()
 	if err != nil {
-		return nil, fmt.Errorf("failed to find database: %v", err)
+		fmt.Println("Database not found, attempting to create...")
+		if createErr := createDatabase(); createErr != nil {
+			return nil, fmt.Errorf("failed to create database: %v", createErr)
+		}
+		conn, err = GetConnection()
+		if err != nil {
+			return nil, fmt.Errorf("failed to establish connection after database creation: %v", err)
+		}
 	}
 	defer conn.Close(context.Background())
 
@@ -31,7 +38,7 @@ func FetchConnections() ([]Instance, error) {
 	for rows.Next() {
 		var inst Instance
 		if err := rows.Scan(&inst.Name, &inst.InstanceId, &inst.Limit, &inst.IsEvo, &inst.Numero); err != nil {
-			fmt.Fprintf(os.Stderr, "Row scan with issues: %v\n", err)
+			fmt.Fprintf(os.Stderr, "Row scan error: %v\n", err)
 			continue
 		}
 		instances = append(instances, inst)
@@ -41,4 +48,21 @@ func FetchConnections() ([]Instance, error) {
 	}
 
 	return instances, nil
+}
+
+func createDatabase() error {
+
+	conn, err := GetConnection()
+	if err != nil {
+		return fmt.Errorf("failed to get admin connection: %v", err)
+	}
+	defer conn.Close(context.Background())
+
+	_, err = conn.Exec(context.Background(), "CREATE DATABASE maturador")
+	if err != nil {
+		return fmt.Errorf("failed to execute create database: %v", err)
+	}
+
+	fmt.Println("Database created successfully.")
+	return nil
 }
